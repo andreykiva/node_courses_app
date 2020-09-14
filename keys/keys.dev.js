68db5603a4896ab118ea73e5cce706946c127be8
Andrey <57412446+podmost777@users.noreply.github.com>
2020-09-15 00:08:55 +0300

    Delete keys.dev.js

--- keys/keys.dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    MONGODB_URI:
-      "mongodb+srv://andrey:HIbsNe0SM2q6QC0x@cluster0-4mr6o.mongodb.net/shop-courses",
-      SESSION_SECRET: "some secret value",
-      SENDGRID_API_KEY: "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",
-      EMAIL_FROM: "andreykiwa@gmail.com",
-      BASE_URL: "http://localhost:3000"
-  };
-  
